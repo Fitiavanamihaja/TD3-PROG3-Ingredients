c8500af995656e4a8913775c57c5a97f2afd8733
Fitiavanamihaja <hei.mickael.122@gmail.com>
2026-01-19 13:43:09 +0300

    Ajoute TD

---
+++ src/main/java/src/main/java/CategoryEnum.java
@@ -0,0 +1,5 @@
+package src.main.java;
+
+public enum CategoryEnum {
+    VEGETABLE, ANIMAL, MARINE, DAIRY, OTHER
+}
---
+++ src/main/java/src/main/java/DishTypeEnum.java
@@ -0,0 +1,5 @@
+package src.main.java;
+
+public enum DishTypeEnum {
+    STARTER, MAIN, DESSERT
+}
